8f46175fb8d3aa0e1d0f126574277d7bd85ce1e9
steven kent <stevenpkent@icloud.com>
2019-12-03 20:51:02 -0500

    add mapHandler

--- main.go
+++ main.go
@@ -8,14 +8,34 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "yo")
 	})
 
-	mux.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/b", func(w http.ResponseWriter, r *http.Request) {
 		url := r.URL.Path
 		fmt.Fprintf(w, "you chose URL "+url)
 	})
 
-	http.ListenAndServe(":8080", mux)
+	urlMap := map[string]string{
+		"/c": "http://www.google.com",
+		"/d": "http://www.gophercises.com",
+	}
+
+	handler := mapHandler(urlMap, mux)
+	http.ListenAndServe(":8080", handler)
+}
+
+func mapHandler(urls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(resp http.ResponseWriter, req *http.Request) {
+		url := req.URL.Path
+		val, present := urls[url]
+
+		if present {
+			http.Redirect(resp, req, val, http.StatusTemporaryRedirect)
+			return
+		}
+
+		fallback.ServeHTTP(resp, req)
+	}
 }
